e5c464aa40de0048c0b10a09a18fd6c9139eab23
Artur Krasiński <artur@minion.pl>
2018-02-21 19:27:31 +0100

    Poprawka danych + old price

--- src/components/Product/ProductPrice.vue
+++ src/components/Product/ProductPrice.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="price-wrapper" :class="{'price-wrapper_special': isSpecial}">
-        <span class="price">{{normalized}}</span>
-        <span class="old" :show="hasOld">{{old}}</span>
+        <span class="price">{{normalizeNumber(value)}}</span>
+        <span class="old" :show="old">{{normalizeNumber(old)}}</span>
     </div>
 </template>
 
@@ -14,14 +14,17 @@
             'old'
         ],
         computed: {
-            normalized () {
-                return ((this.value * 100) / 100).toFixed(2)
-            },
             isSpecial () {
                 return this.type === 'special'
-            },
-            hasOld () {
-                return this.old
+            }
+        },
+        methods: {
+            normalizeNumber (numberToNormalize) {
+                let normalizedNumber
+                if (numberToNormalize) {
+                    normalizedNumber = ((numberToNormalize * 100) / 100).toFixed(2)
+                }
+                return normalizedNumber
             }
         }
     }
@@ -29,14 +32,33 @@
 
 <style scoped>
     .price-wrapper {
-        width: .calc(100%-20px);
+        position: relative;
+        width: calc(100% - 20px);
         padding: 10px 0;
         margin: 0 10px;
         text-align: center;
     }
-    .price-wrapper_special{
+    .price-wrapper_special {
         background: rgba(255,255,0,0.9);
         color: #000000;
         font-weight: 700;
+    }
+    .old {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        opacity: 0.7;
+        font-size: 0.9;
+        transform: rotate(-30deg);
+    }
+    .old::after{
+        width: 120%;
+        height: 2px;
+        content: " ";
+        background: red;
+        display: block;
+        position: absolute;
+        top: calc(50% - 2px);
+        left: -10%;
     }
 </style>
